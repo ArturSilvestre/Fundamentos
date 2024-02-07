e4add5c30eef269478db2a0c24120bae95ca4813
Artur Oliveira <artur.oliveira@avenuecode.com>
2024-02-07 18:14:07 -0300

    :lipstick: feat: Created stylization of the main structure of the page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
   return (
     <>
       <Header />
 
-      <Post 
-        author ="Artur Silvestre"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit deleniti possimus a soluta fuga accusantium, commodi maxime odio non pariatur illum nostrum necessitatibus praesentium, reprehenderit dignissimos minus nesciunt aspernatur architecto" 
-      />
-      <Post 
-        author ="Diego Fernandes"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit deleniti possimus a soluta fuga accusantium, commodi maxime odio non pariatur illum nostrum necessitatibus praesentium, reprehenderit dignissimos minus nesciunt aspernatur architecto" 
-      />
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          <Post 
+          author ="Artur Silvestre"
+          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit deleniti possimus a soluta fuga accusantium, commodi maxime odio non pariatur illum nostrum necessitatibus praesentium, reprehenderit dignissimos minus nesciunt aspernatur architecto" 
+        />
+        <Post 
+          author ="Diego Fernandes"
+          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit deleniti possimus a soluta fuga accusantium, commodi maxime odio non pariatur illum nostrum necessitatibus praesentium, reprehenderit dignissimos minus nesciunt aspernatur architecto" 
+        />
+        </main>
+      </div>
     </>
   )
 }
